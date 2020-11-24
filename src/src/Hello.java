ed07af8ef77d4beb0a3f2f9798bd2aa787df9b34
tarunmittal <mittal.tarun15@gmail.com>
2020-11-24 16:00:11 +0530

    Primitive

--- src/src/Hello.java
+++ src/src/Hello.java
@@ -5,9 +5,14 @@ public class Hello {
 
         int myFirstNumber = (10 + 5) + (2 * 10); // Declaration statement.
         int mySecondNumber = 12;
-        int myThirdNumber = 6;
+        int myThirdNumber = myFirstNumber * 2;
         int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
-
         System.out.println("The total is :" + myTotal);
+
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println("The value for the last one is :" + myLastOne);
+
+
     }
 }
---
+++ src/src/Main.java
@@ -0,0 +1,14 @@
+import java.net.InetAddress;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("This is the minimum value for an integer: " + myMinIntValue);
+        System.out.println("This is the maximum value for an integer: " + myMaxIntValue);
+    }
+}
