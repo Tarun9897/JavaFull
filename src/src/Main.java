0f98f6073df54620ba03ba3341669f8ccc86f2d8
tarunmittal <mittal.tarun15@gmail.com>
2020-11-24 16:06:11 +0530

    Primitive Completed

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,4 +1,3 @@
-import java.net.InetAddress;
 
 public class Main {
 
@@ -10,5 +9,8 @@ public class Main {
         int myMaxIntValue = Integer.MAX_VALUE;
         System.out.println("This is the minimum value for an integer: " + myMinIntValue);
         System.out.println("This is the maximum value for an integer: " + myMaxIntValue);
+        System.out.println("Busted max value: " + (myMaxIntValue + 1)); // Overflow
+        System.out.println("Busted min value: " + (myMinIntValue - 1)); //Underflow
+
     }
 }
