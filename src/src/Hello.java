2a25fbf20ec2782b6565911991e152b8cc7ebcfa
tarunmittal <mittal.tarun15@gmail.com>
2020-11-24 15:49:16 +0530

    Expressions

--- src/src/Hello.java
+++ src/src/Hello.java
@@ -1,10 +1,13 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello Tarun");
+        System.out.println("Hello, Tarun");
 
-        int myFirstNumber = 5; // Declaration statement.
+        int myFirstNumber = (10 + 5) + (2 * 10); // Declaration statement.
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
 
-        System.out.println("My first number is :" + myFirstNumber);
+        System.out.println("The total is :" + myTotal);
     }
 }
