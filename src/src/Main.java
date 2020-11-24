1585688f4b767862a6d18fd7acb81b6dd880d1a5
tarunmittal <mittal.tarun15@gmail.com>
2020-11-24 16:15:59 +0530

    Primitive Completed full

--- src/src/Main.java
+++ src/src/Main.java
@@ -7,10 +7,34 @@ public class Main {
 
         int myMinIntValue = Integer.MIN_VALUE;
         int myMaxIntValue = Integer.MAX_VALUE;
+
         System.out.println("This is the minimum value for an integer: " + myMinIntValue);
         System.out.println("This is the maximum value for an integer: " + myMaxIntValue);
         System.out.println("Busted max value: " + (myMaxIntValue + 1)); // Overflow
         System.out.println("Busted min value: " + (myMinIntValue - 1)); //Underflow
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+
+        System.out.println("Byte Minimum value: " +myMinByteValue);
+        System.out.println("Byte Maximum value: " +myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+
+        System.out.println("Short Minimum value: " +myMinShortValue);
+        System.out.println("Short Maximum value: " +myMaxShortValue);
+        
+        long myLongValue = 100L;
+
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+
+        System.out.println("Long Minimum value: " +myMinLongValue);
+        System.out.println("Long Maximum value: " +myMaxLongValue);
+
+
+
 
     }
 }
