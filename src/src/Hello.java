734daa48d8c788de67299d17c89535bfd6e3c0a9
tarunmittal <mittal.tarun15@gmail.com>
2020-11-24 15:43:36 +0530

    Variables

--- src/src/Hello.java
+++ src/src/Hello.java
@@ -2,5 +2,9 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello Tarun");
+
+        int myFirstNumber = 5; // Declaration statement.
+
+        System.out.println("My first number is :" + myFirstNumber);
     }
 }
